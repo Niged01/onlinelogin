33996b8805638e458c4d85d09f5762efb8173755
Niged01 <nigedoberer@gmail.com>
2022-06-07 18:50:47 +0000

    Refactored validation

--- src/index.js
+++ src/index.js
@@ -1,125 +1,16 @@
+
 const username = document.querySelector('#username');
 const password = document.querySelector('#password');
 
-const form = document.getElementById('btn');
-
-
-const checkUsername = () => {
-
-    let valid = false;
-
-    const min = 3,
-        max = 25;
-
-    const username = username.value.trim();
-
-    if (!isRequired(username)) {
-        showError(username, 'Username cannot be blank.');
-    } else if (!isBetween(username.length, min, max)) {
-        showError(username, `Username must be between ${min} and ${max} characters.`)
-    } else {
-        showSuccess(username);
-        valid = true;
-    }
-    return valid;
-};
-
-const checkPassword = () => {
-    let valid = false;
-
-
-    const password = password.value.trim();
-
-    if (!isRequired(password)) {
-        showError(password, 'Password cannot be blank.');
-    } else if (!isPasswordSecure(password)) {
-        showError(password, 'Password must has at least 8 characters that include at least 1 lowercase character, 1 uppercase characters, 1 number, and 1 special character in (!@#$%^&*)');
-    } else {
-        showSuccess(password);
-        valid = true;
-    }
-
-    return valid;
-};
-
-
-const isPasswordSecure = (password) => {
-    const re = new RegExp(password);
-    return re.test(password);
-};
-
-const isRequired = value => value === '' ? false : true;
-const isBetween = (length, min, max) => length < min || length > max ? false : true;
-
-
-const showError = (input, message) => {
-    // get the form-field element
-    const formField = input.parentElement;
-    // add the error class
-    formField.classList.remove('success');
-    formField.classList.add('error');
-
-    // show the error message
-    const error = formField.querySelector('small');
-    error.textContent = message;
-};
-
-const showSuccess = (input) => {
-    // get the form-field element
-    const formField = input.parentElement;
-
-    // remove the error class
-    formField.classList.remove('error');
-    formField.classList.add('success');
-
-    // hide the error message
-    const error = formField.querySelector('small');
-    error.textContent = '';
-}
-
-
-form.addEventListener('submit', function (e) {
-    // prevent the form from submitting
-    e.preventDefault();
-
-    // validate fields
-    let isUsernameValid = checkUsername(),
-        isPasswordValid = checkPassword();
-
-    let isFormValid = isUsernameValid &&
-        isPasswordValid;
-
-    // submit to the server if the form is valid
-    if (isFormValid) {
-
-    }
-});
-
-
-const debounce = (fn, delay = 500) => {
-    let timeoutId;
-    return (...args) => {
-        // cancel the previous timer
-        if (timeoutId) {
-            clearTimeout(timeoutId);
-        }
-        // setup a new timer
-        timeoutId = setTimeout(() => {
-            fn.apply(null, args)
-        }, delay);
+function validateform() {
+if (username==null || username==""){  
+    alert("Name can't be blank");  
+    return false;  
+  }else if(password.length<6){  
+    alert("Password must be at least 6 characters long.");  
+    return false;  
     };
-};
-
-form.addEventListener('input', debounce(function (e) {
-    switch (e.target.id) {
-        case 'username':
-            checkUsername();
-            break;
-        case 'password':
-            checkPassword();
-            break;
-    }
-}));
+}
 
 
 var user = {
@@ -129,3 +20,5 @@ var user = {
 var json = JSON.stringify(user);
 localStorage.setItem('username', json);
 console.log('username');
+
+validateform()
